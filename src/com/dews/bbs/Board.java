96ce4b9f9da02b9d93883ad94ca6fd707da6b18d
DDews <danielricharddews@gmail.com>
2016-04-11 13:42:29 -0600

    Added sql file for getting information about the boards, # posts, # threads, last post, etc

--- src/com/dews/bbs/Board.java
+++ src/com/dews/bbs/Board.java
@@ -6,7 +6,35 @@ public class Board {
 	private String name;
 	private String description;
 	private int id;
+	private int posts;
+	private int topics;
+	private String recentTopic;
+	private int recentTopicId;
 	public Board() {}
+	public void setRecentTopic(String recentTopic) {
+		this.recentTopic = recentTopic;
+	}
+	public String getRecentTopic() {
+		return recentTopic;
+	}
+	public void setRecentTopiicId(int id) {
+		this.recentTopicId = id;
+	}
+	public int getRecentTopicId() {
+		return recentTopicId;
+	}
+	public void setPosts(int posts) {
+		this.posts = posts;
+	}
+	public int getPosts() {
+		return posts;
+	}
+	public void setTopics(int topics) {
+		this.topics = topics;
+	}
+	public int getTopics() {
+		return topics;
+	}
 	public void setName(String name) {
 		this.name = name;
 	}
--- src/com/dews/bbs/Hello.java
+++ src/com/dews/bbs/Hello.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.PathParam;
 
 import java.io.IOException;
@@ -30,18 +31,79 @@ import java.util.List;
 @Path("/hello")
 public class Hello {
 	@POST
-	  @Produces(MediaType.TEXT_HTML)
-	  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	  public void newTodo(@FormParam("id") String id,
-	      @FormParam("name") String name,
-	      @FormParam("desc") String desc,
-	      @Context HttpServletResponse servletResponse) throws IOException {
-	    System.out.println("name: " + name + ", desc: " + desc);
-	
-	    servletResponse.sendRedirect("../create_todo.html");
-	  }
+	@Path("/login")
+	  @Consumes(MediaType.APPLICATION_JSON)
+	  public Response login(User user) throws IOException, ClassNotFoundException, SQLException {
+	    System.out.println(user.toString());
+	    Class.forName("org.postgresql.Driver");
+		  String url = "jdbc:postgresql://localhost/bbs";
+		  Properties props = new Properties();
+		  props.setProperty("user","postgres");
+		  props.setProperty("password","moondied");
+		  Connection conn = DriverManager.getConnection(url, props);
+		  PreparedStatement pst = null;
+		  String stm = "SELECT * FROM users WHERE username = ?";
+          pst = conn.prepareStatement(stm);
+          pst.setString(1, user.getUsername());
+		  ResultSet rs = pst.executeQuery();
+		  rs.next();
+		  System.out.println(rs.getInt(1));
+		  System.out.println(rs.getString(2));
+		  System.out.println(rs.getBoolean(3));
+		  System.out.println(rs.getString(4));
+		  if (BCrypt.checkpw(user.getPassword(), rs.getString(4))) return Response.status(200).entity(rs.getString(4)).build();
+		  if (rs.getString(4).equals(user.getPassword())) return Response.status(200).entity(rs.getString(4)).build();
+	    return Response.serverError().entity("Incorrect password").build();
+	}
+	@POST
+	@Path("/register")
+	  @Consumes(MediaType.APPLICATION_JSON)
+	  public Response createBoard(User user) throws IOException, ClassNotFoundException {
+	    System.out.println(user.toString());
+	    Class.forName("org.postgresql.Driver");
+	    Connection con = null;
+        PreparedStatement pst = null;
+
+        String url = "jdbc:postgresql://localhost/bbs";
+        String pw_hash = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+        try {
+            Properties props = new Properties();
+      	  	props.setProperty("user","postgres");
+      	  	props.setProperty("password","moondied");
+      	  	con = DriverManager.getConnection(url, props);
+
+            String stm = "INSERT INTO users(username, admin, password) VALUES(?, ?, ?)";
+            pst = con.prepareStatement(stm);
+            pst.setString(1, user.getUsername());
+            pst.setBoolean(2,true);
+            pst.setString(3, pw_hash);
+            pst.executeUpdate();
+            System.out.println("Added: " + user.getUsername() + " password hash: " + pw_hash);
+
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.toString());
+            if (ex.getMessage().contains("already exists")) return Response.serverError().entity("Username already exists").build();
+            return Response.serverError().entity(ex.toString()).build();
+
+        } finally {
+
+            try {
+                if (pst != null) {
+                    pst.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.toString());
+                return Response.serverError().entity(ex.toString()).build();
+            }
+        }
+	    return Response.status(200).entity(pw_hash).build();
+	}
   @GET
-  @Path("/{id}")
+  @Path("/boards")
   @Produces(MediaType.APPLICATION_JSON)
   public List<Board> getBoards(@PathParam("id") String id) throws SQLException, ClassNotFoundException {
 	  Class.forName("org.postgresql.Driver");
